74347f35d9260a556a173f221649ba998cd21300
Felipe <philbahia@hotmail.com>
2023-03-07 10:46:29 -0300

    final task2

--- assets/scripts/details.js
+++ assets/scripts/details.js
@@ -1,15 +1,42 @@
 import data from "./amazing.js";
 
-let detalle = document.getElementById("detail-li");
-console.log(data.currentDate);
 
-let imagen = data.events[7].image;
-console.log(data.events[7].category);
-console.log(imagen);
+//recupero fecha
+const date_today = data.currentDate;
+console.log(date_today);
 
-let imgCardDetail = document.getElementById("bigcard")
+//desestructura object data
+let { events } = data;
+//creamos array de objetos events
+console.log("array eventos");
+console.log(events);
 
-imgCardDetail.src = imagen;
+let pastEvents = events.filter(ep => ep.date > date_today);
 
-const prueba = data.events;
-console.table(prueba);
+console.log("array eventos filtrado");
+    console.table(pastEvents);
+
+//categorias
+
+
+// *************************************************
+
+
+const contcard = document.querySelector("#cardMain");
+const template = document.querySelector('#card-tpl').content;
+const fragment = document.createDocumentFragment() ;
+
+pastEvents.forEach(event => {
+    template.querySelector('.card-img-top').src = event.image;
+    template.querySelector('.card-title').textContent = event.name;
+    template.querySelector('.card-text').textContent = event.description;
+    template.querySelector('.card-price').textContent = "$ " + event.price;
+    const clone = template.cloneNode(true);
+    fragment.appendChild(clone);
+
+})
+
+contcard.appendChild(fragment);
+
+
+
